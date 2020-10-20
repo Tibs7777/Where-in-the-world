7be673b67f0ceed664827e9d8d176261f63abf73
Tibs7777 <tom.bartlett2@btinternet.com>
2020-10-20 21:35:32 +0100

    animation fixes

--- src/components/ui/Flag/Flag.js
+++ src/components/ui/Flag/Flag.js
@@ -7,23 +7,23 @@ import './Flag.scss'
 export default function Flag(props) {
    const flag = useRef(null)
 
-//    const setAnim = () => {
-//        flag.current.style.transform= `translateX(${document.querySelector('.Flags').offsetWidth - window.innerWidth}px)`
-//    }
-//     useEffect(() => {
-//         console.log(document.querySelector('.Flags').offsetWidth)
-//         flag.current.style.transform= `translateX(${document.querySelector('.Flags').offsetWidth - window.innerWidth}px)`
-//     }, [])
+   const setAnim = () => {
+       flag.current.style.transform= `translateX(${document.querySelector('.Flags').offsetWidth - window.innerWidth}px)`
+   }
+    // useEffect(() => {
+    //     console.log(document.querySelector('.Flags').offsetWidth)
+    //     flag.current.style.transform= `translateX(${document.querySelector('.Flags').offsetWidth - window.innerWidth}px)`
+    // }, [])
 
 
-    // const transitionEndHandler = (e) => {
-    //     // if(e.propertyName === "transform") {
-    //     //     console.log('resetting')
-    //     //     flag.current.style.transition = "none"
-    //     //     flag.current.style.transform= `translateX(0)`
-    //     //     setAnim()
-    //     // }
-    // }
+    const transitionEndHandler = (e) => {
+        // if(e.propertyName === "transform") {
+        //     console.log('resetting')
+        //     flag.current.style.transition = "none"
+        //     flag.current.style.transform= `translateX(0)`
+        //     setAnim()
+        // }
+    }
 
 
 
